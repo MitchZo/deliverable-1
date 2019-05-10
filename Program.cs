dffa7230e0b4d5ea887f4d14c62dc3266393ff59
Mitch <outvisible@gmail.com>
2019-05-09 21:02:55 -0400

    Day 1. loop to accept first value

--- Program.cs
+++ Program.cs
@@ -6,7 +6,24 @@ namespace Currency_Format_Converter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal amt1 = 0;
+            decimal amt2 = 0;
+            decimal amt3 = 0;
+
+            Console.WriteLine("Welcome to the adding machine. Please enter your first value in the format of XXX.XX.");
+            string response = Console.ReadLine();
+
+
+                if (decimal.TryParse(response, out amt1))
+                {
+                amt1 = decimal.Parse(response);
+                 Console.WriteLine("Please enter your second number in the format of XXX.XX.");
+                }
+                else
+                {
+                Console.WriteLine("That is not the correct format. Please try again.");
+                Console.ReadLine()
+                }
         }
     }
 }
