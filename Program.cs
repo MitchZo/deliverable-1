422dc07066ba87a078ef45f1a3359373271dc726
Mitch <outvisible@gmail.com>
2019-05-09 20:02:51 -0400

    but this time though...

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Currency_Format_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
