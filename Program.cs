be6e7ee6e27f9b6656f7f2026d3ed93ffb181387
Mitch <outvisible@gmail.com>
2019-05-11 13:52:47 -0400

    While Try Catch amt1

--- Program.cs
+++ Program.cs
@@ -6,34 +6,22 @@ namespace Currency_Format_Converter
     {
         static void Main(string[] args)
         {
-            decimal amt1 = 0;
-            decimal amt2 = 0;
-            decimal amt3 = 0;
-            int value = 1;
-            string response;
+            Decimal? amt1 = null;
+            Decimal? amt2 = null;
+            Decimal? amt3 = null;
 
-            Console.WriteLine("Welcome to the adding machine. Please enter value #{0} in the format of XXX.XX.", value);
-            response = Console.ReadLine();
-
-            try
+            while (amt1 == null) 
             {
-                amt1 = decimal.Parse(response);
-            }
-            catch (Exception)
-            {
-                while (decimal.TryParse(response, out amt1))
+                try
+                {
+                    Console.WriteLine("Welcome to the adding machine. Please enter value #1 in the format of XXX.XX.";
+                    string response = Console.ReadLine();
+                    amt1 = decimal.Parse(response);
+                }
+                catch (Exception)
                 {
                     Console.WriteLine("That is not the correct format. Please try again.");
-                    //remove "+ amt1" before submitting 
-                    Console.WriteLine("Please enter value #{0} in the format of XXX.XX." + amt1, value);
-                    response = Console.ReadLine();
                 }
-            }
-            finally
-            {
-                    value ++;
-                //remove "+ amt1" before submitting
-                Console.WriteLine("Please enter value #{0} in the format of XXX.XX." + amt1, value);
             }
         }
     }
